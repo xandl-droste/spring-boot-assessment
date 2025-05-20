0c513680afb16ce921f4df8600897d53dd11fd68
Alex Droste <alex.droste@gmx.at>
2025-05-20 03:18:36 +0200

    fooling around

--- src/main/java/com/example/springbootassessment/task/web/controller/TaskController.java
+++ src/main/java/com/example/springbootassessment/task/web/controller/TaskController.java
@@ -1,33 +1,47 @@
 package com.example.springbootassessment.task.web.controller;
 
-import com.example.springbootassessment.task.web.dto.TaskDto;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.springbootassessment.task.service.TaskService;
+import com.example.springbootassessment.task.web.dto.TaskDto;
+import com.example.springbootassessment.task.web.mapper.TaskMapper;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
-    // todo: inject the task service, that loads the tasks from the database
-    /*
-    there are two implementations of the same TaskService interface, we need to ensure
-    that the right one is used by spring DI here!
-     */
+	// todo: inject the task service, that loads the tasks from the database
+	/*
+	 * there are two implementations of the same TaskService interface, we need to
+	 * ensure that the right one is used by spring DI here!
+	 */
+	private final TaskService taskService;
+	private final TaskMapper taskMapper;
 
-    @GetMapping
-    public ResponseEntity<List<TaskDto>> getAllTasks() {
-        throw new RuntimeException("todo: implement");
-    }
+//	@Autowired
+//	public TaskController(TaskService taskService, TaskMapper taskMapper) {
+//		super();
+//		this.taskService = taskService;
+//		this.taskMapper = taskMapper;
+//	}
 
-    @GetMapping("/{projectId}")
-    public ResponseEntity<List<TaskDto>> getTasksByProjectId(@PathVariable Long projectId) {
-        throw new RuntimeException("todo: implement");
-    }
+	@GetMapping
+	public ResponseEntity<List<TaskDto>> getAllTasks() {
+		List<TaskDto> taskDtoList = taskService.findAll().stream().map(taskMapper::map).toList();
+		return ResponseEntity.ok(taskDtoList);
+	}
+
+	@GetMapping("/{projectId}")
+	public ResponseEntity<List<TaskDto>> getTasksByProjectId(@PathVariable Long projectId) {
+		throw new RuntimeException("todo: implement");
+	}
 }
--- src/test/java/com/example/springbootassessment/task/web/controller/TaskIntegrationTest.java
+++ src/test/java/com/example/springbootassessment/task/web/controller/TaskIntegrationTest.java
@@ -1,60 +1,77 @@
 package com.example.springbootassessment.task.web.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.fail;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import com.example.springbootassessment.project.repository.ProjectRepository;
+import com.example.springbootassessment.task.repository.TaskRepository;
+import com.example.springbootassessment.task.web.dto.TaskDto;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@Sql(value = "/data/setup-task-with-project.sql", executionPhase = BEFORE_TEST_METHOD)
 class TaskIntegrationTest {
 
-    @Autowired
-    // resources on how to use in https://docs.spring.io/spring-framework/reference/testing/webtestclient.html#webtestclient-no-content
-    private WebTestClient webTestClient;
+	@Autowired
+	// resources on how to use in
+	// https://docs.spring.io/spring-framework/reference/testing/webtestclient.html#webtestclient-no-content
+	private WebTestClient webTestClient;
 
-    @AfterEach
-    void tearDown() {
-        // todo: make sure that after the test all data is clean
-    }
+	@Autowired
+	TaskRepository taskRepository;
 
-    @Test
-    @DisplayName("should load all tasks")
-    void shouldLoadAllTasks() {
-        // given
+	@Autowired
+	ProjectRepository projectRepository;
 
-        // when
+	@AfterEach
+	void tearDown() {
+		taskRepository.deleteAll();
+		projectRepository.deleteAll();
+	}
 
-        // then
-        fail("todo: implement");
-    }
+	@Test
+	@DisplayName("should load all tasks")
+	void shouldLoadAllTasks(TestInfo testInfo) {
+		System.out.println("Test: " + testInfo.getDisplayName());
+		System.out.println("Tasks in DB: " + taskRepository.count());
 
-    @Test
-    @DisplayName("should load task by project id")
-    @Sql(value = "/data/setup-task-with-project.sql", executionPhase = BEFORE_TEST_METHOD)
-    void shouldLoadTasksByProjectId() {
-        // given
+		List<TaskDto> taskDtoList = webTestClient.get().uri("tasks").exchange().expectStatus().is2xxSuccessful()
+				.expectBodyList(TaskDto.class).returnResult().getResponseBody();
 
-        // when
+		assertThat(taskDtoList).hasSize(5);
+	}
 
-        // then
-        fail("todo: implement");
-    }
+	@Test
+	@DisplayName("should load task by project id")
+	void shouldLoadTasksByProjectId() {
+		// given
 
-    @Test
-    @DisplayName("given project id does not exist, when loading tasks by project id, then should return 404")
-    void givenProjectIdDoesNotExistWhenLoadingTasksByProjectIdThenShouldReturn404() {
-        // given
+		// when
 
-        // when
+		// then
+		fail("todo: implement");
+	}
 
-        // then
-        fail("todo: implement");
-    }
+	@Test
+	@DisplayName("given project id does not exist, when loading tasks by project id, then should return 404")
+	void givenProjectIdDoesNotExistWhenLoadingTasksByProjectIdThenShouldReturn404() {
+		// given
+
+		// when
+
+		// then
+		fail("todo: implement");
+	}
 }
