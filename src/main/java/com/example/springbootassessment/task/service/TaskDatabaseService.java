4273c1540c8803db0e1714dd36ddd6d296188421
Alex Droste <alex.droste@gmx.at>
2025-05-20 03:16:54 +0200

    fooling around

--- src/main/java/com/example/springbootassessment/task/service/TaskDatabaseService.java
+++ src/main/java/com/example/springbootassessment/task/service/TaskDatabaseService.java
@@ -1,22 +1,31 @@
 package com.example.springbootassessment.task.service;
 
-import com.example.springbootassessment.task.domain.Task;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.springbootassessment.task.domain.Task;
+import com.example.springbootassessment.task.domain.mapper.TaskEntityMapper;
+import com.example.springbootassessment.task.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@Profile("db")
 @RequiredArgsConstructor
 public class TaskDatabaseService implements TaskService {
 
-    @Override
-    public List<Task> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	private final TaskRepository taskRepository;
+	private final TaskEntityMapper taskEntityMapper;
 
-    @Override
-    public List<Task> findAllByProjectId(Long projectId) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public List<Task> findAll() {
+		return taskRepository.findAll().stream().map(taskEntityMapper::map).toList();
+	}
+
+	@Override
+	public List<Task> findAllByProjectId(Long projectId) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
--- src/main/java/com/example/springbootassessment/task/service/TaskWebService.java
+++ src/main/java/com/example/springbootassessment/task/service/TaskWebService.java
@@ -1,24 +1,28 @@
 package com.example.springbootassessment.task.service;
 
-import com.example.springbootassessment.task.domain.Task;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.springbootassessment.task.domain.Task;
+
+import lombok.RequiredArgsConstructor;
+
 @Primary
+@Profile("web")
 @Service
 @RequiredArgsConstructor
 public class TaskWebService implements TaskService {
 
-    @Override
-    public List<Task> findAll() {
-        throw new RuntimeException("Task Webservice is currently not supported");
-    }
+	@Override
+	public List<Task> findAll() {
+		throw new RuntimeException("Task Webservice is currently not supported");
+	}
 
-    @Override
-    public List<Task> findAllByProjectId(Long projectId) {
-        throw new RuntimeException("Task Webservice is currently not supported");
-    }
+	@Override
+	public List<Task> findAllByProjectId(Long projectId) {
+		throw new RuntimeException("Task Webservice is currently not supported");
+	}
 }
